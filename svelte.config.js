51078b7f81e42294fd53780dace66543c0c78100
Kyree Jonas <contact@kre3.net>
2026-02-23 01:15:20 +0800

    Fix: 404 page not working

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ const config = {
       // these options are set automatically — see below
       pages: 'build',
       assets: 'build',
-      fallback: undefined,
+      fallback: '404.html',
       precompress: false,
       strict: true
     })
